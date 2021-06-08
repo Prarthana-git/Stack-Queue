23c6f8db5d0b625bd5a476d59ce03aed090ff763
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-08 22:27:23 +0530

    Added Dequeque

--- src/test/java/com/stackandqueque/MyQueueTest.java
+++ src/test/java/com/stackandqueque/MyQueueTest.java
@@ -4,20 +4,40 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyQueueTest {
-    /**
-     * this test case will check the enqueue,printqueue and peak method.
-     */
-    @Test
-    public void given3NumberWhenAddedToQueueShouldHaveFirstAddedNode() {
-        MyNode<Integer> myFirstNode = new MyNode<>(56);
-        MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myThirdNode = new MyNode<>(70);
-        MyQueue myQueue = new MyQueue();
-        myQueue.enqueue(myFirstNode);
-        myQueue.enqueue(mySecondNode);
-        myQueue.enqueue(myThirdNode);
-        myQueue.printQueue();
-        INode peak = myQueue.peak();
-        Assert.assertEquals(myFirstNode,peak);
-    }
+	/**
+	 * this test case will check the enqueue,printqueue and peak method.
+	 */
+	@Test
+	public void given3NumberWhenAddedToQueueShouldHaveFirstAddedNode() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyQueue myQueue = new MyQueue();
+		myQueue.enqueue(myFirstNode);
+		myQueue.enqueue(mySecondNode);
+		myQueue.enqueue(myThirdNode);
+		myQueue.printQueue();
+		INode peak = myQueue.peak();
+		Assert.assertEquals(myFirstNode, peak);
+	}
+
+	/**
+	 * this method will check the dequeue method.
+	 */
+	@Test
+	public void given3NumberWhenDeletedToQueueShouldHaveLastAddedNode() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyQueue myQueue = new MyQueue();
+		myQueue.enqueue(myFirstNode);
+		myQueue.enqueue(mySecondNode);
+		myQueue.enqueue(myThirdNode);
+		myQueue.dequeue();
+		myQueue.printQueue();
+		myQueue.dequeue();
+		myQueue.printQueue();
+		INode peak = myQueue.peak();
+		Assert.assertEquals(myThirdNode, peak);
+	}
 }
