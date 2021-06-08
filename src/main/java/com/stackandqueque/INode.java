3b9dd89e9e763b11a20348eee6b74f603631fed4
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-08 22:01:51 +0530

    Added stack-queue

---
+++ src/main/java/com/stackandqueque/INode.java
@@ -0,0 +1,15 @@
+package com.stackandqueque;
+
+/**
+ * Here i have created a generic interface.
+ * have two methods get value and set value.
+ * @param <K>
+ */
+public interface INode<K> {
+    K getKey();
+    void setKey(K Key);
+
+    INode<K> getNext();
+    void setNext(INode<K> next);
+}
+
---
+++ src/test/java/com/stackandqueque/MyStackTest.java
@@ -0,0 +1,31 @@
+package com.stackandqueque;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+/**
+ * here i have created a test class MyStackTest.
+ */
+
+public class MyStackTest 
+{
+	 /**
+     * this will check the stack has created or not.
+     * this will check the push,printstack methods.
+     */
+    @Test
+    public void given3NumberWhenAddedToStackShouldHaveLastAddedNode() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyStack myStack = new MyStack();
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        myStack.printStack();
+        INode peak = myStack.peak();
+        Assert.assertEquals(myThirdNode,peak);
+}
+    
+}
